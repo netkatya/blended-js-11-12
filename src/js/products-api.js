52d333032f841c72126e5c14ff4e81901c6801d7
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-06-14 20:41:03 +0100

    modified

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,24 @@
 // Функції для роботи з бекендом
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://dummyjson.com";
+
+
+export async function fetchCategories() {
+    const { data } = await axios.get("/products/category-list");
+    return ["All", ...data];
+}
+
+export async function fetchProducts(page = 1, limit = 12) {
+    const skip = (page - 1) * limit;
+    const { data } = await axios.get(`/products?limit=${limit}&skip=${scip}`);
+    return data.products;
+}
+
+
+axios.get("/docs/products").then().catch();
+axios.get("/products?limit=10&skip=10 ").then().catch();
+axios.get("/products/1").then().catch();
+axios.get("/products/search?q=nail").then().catch();
+
+axios.get("/products/category/smartphones").then().catch();
